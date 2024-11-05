08824e824f97edef5197ff3833b614334c77c4eb
sebasmayor06 <sebda06@gmail.com>
2024-11-05 13:33:08 -0500

    corrijo cors

--- src/index.js
+++ src/index.js
@@ -9,13 +9,27 @@ const app = express();
 // Lista de orígenes permitidos
 const allowedOrigins = [
   'http://localhost:5173', // Para desarrollo
-  'https://asistencia-cci-front-dc13af93f3e4.herokuapp.com/' // Para producción
+  'https://asistencia-cci-front-dc13af93f3e4.herokuapp.com' // Para producción (sin barra al final)
 ];
 
 // Configuración de CORS
 app.use(cors({
     origin: function (origin, callback) {
-        // Permitir solicitudes sin 'origin' (como mobile apps o curl requests)
+        if (!origin) return callback(null, true); // Permitir solicitudes sin 'origin'
+        
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        
+        return callback(null, true);
+    },
+    credentials: true
+}));
+
+// Manejo de solicitudes preflight para cualquier ruta
+app.options('*', cors({
+    origin: function (origin, callback) {
         if (!origin) return callback(null, true);
         
         if (allowedOrigins.indexOf(origin) === -1) {
