687062c7612efebee2d4ff3d7903ed32dfe06d6e
sebasmayor06 <sebda06@gmail.com>
2024-11-05 11:11:16 -0500

    arreglo cors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
-import express from 'express'
-import userRoutes from './routes/user.routes.js'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import morgan from 'morgan'
+import express from 'express';
+import userRoutes from './routes/user.routes.js';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
 
+const app = express();
 
+// Lista de orígenes permitidos
+const allowedOrigins = [
+  'http://localhost:5173', // Para desarrollo
+  'https://asistencia-cci-front-dc13af93f3e4.herokuapp.com' // Para producción
+];
 
-const app = express()
+// Configuración de CORS
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+        // Permitir solicitudes sin 'origin' (como mobile apps o curl requests)
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        
+        return callback(null, true);
+    },
     credentials: true
-}))
+}));
+
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(morgan('dev'));
-app.use(userRoutes)
+app.use(userRoutes);
 
-
-app.listen(3000, () => {
-
-    console.log('Server is listening on port: ', 3000);
-})
-
+// Usar process.env.PORT para permitir que Heroku asigne el puerto
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`);
+});
