0f6986708bf4fe3e6f31235907d3807828480709
sebasmayor06 <sebda06@gmail.com>
2024-11-10 07:45:41 -0500

    agrego consultas para tablas

---
+++ src/controllers/ConsultarAsistenciaEvent.js
@@ -0,0 +1,24 @@
+import pool from "../db.js";
+
+export const consultarAsistenciaEvent = async (req, res) => {
+
+  const {eventId} = req.body.event_id
+
+  console.log(eventId);
+  
+
+  
+  try {
+    const sql = `SELECT * FROM asistencia WHERE event_id = ${eventId}`;
+    const { rows } = await pool.query(sql);
+
+    if (rows.length > 0) {
+      res.status(200).json(rows);
+    } else {
+      res.status(200).json(''); 
+    }
+  } catch (error) {
+    console.error('Error en la consulta:', error);
+    res.status(500).json({ message: 'Error en el servidor' });
+  }
+};
