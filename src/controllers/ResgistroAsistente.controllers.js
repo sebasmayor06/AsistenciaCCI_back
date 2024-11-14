639237a6262aaad6c59b16d25f0aa73c1ab04fe8
sebasmayor06 <sebda06@gmail.com>
2024-11-14 08:10:11 -0500

    subo cambios pedidos por el pastor

--- src/controllers/ResgistroAsistente.controllers.js
+++ src/controllers/ResgistroAsistente.controllers.js
@@ -4,12 +4,10 @@ import  pool  from "../db.js";
 
 export const registerAsistente = async (req,res) =>{
 
-    const {dni, full_name, estado_civil, phone_number, fecha_de_nacimiento, ciudad, barrio, direccion, bautizo} = req.body;
-    const sql = `INSERT INTO asistentes (dni, full_name, estado_civil, phone_number, fecha_de_nacimiento, ciudad, barrio, direccion, bautizo) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`
+    const {dni, full_name, estado_civil, phone_number, fecha_de_nacimiento, ciudad, barrio, direccion, bautizo, registration_time} = req.body;
+    const sql = `INSERT INTO asistentes (dni, full_name, estado_civil, phone_number, fecha_de_nacimiento, ciudad, barrio, direccion, bautizo, registration_time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`
 
-    console.log(" SQL resg asistente: ", sql);
-
-    const {rows} = await pool.query(sql, [dni, full_name, estado_civil, phone_number, fecha_de_nacimiento, ciudad, barrio, direccion, bautizo])
+    const {rows} = await pool.query(sql, [dni, full_name, estado_civil, phone_number, fecha_de_nacimiento, ciudad, barrio, direccion, bautizo, registration_time])
     res.status(200).json(rows[0])
 
 }
