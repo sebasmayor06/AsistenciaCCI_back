258a419dd451d1377db05d2232a724e94e61bb0b
sebasmayor06 <sebda06@gmail.com>
2024-11-20 15:30:50 -0500

    cors para iglesiacci.com

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-import express from 'express'
-import userRoutes from './routes/user.routes.js'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import morgan from 'morgan'
+import express from 'express';
+import userRoutes from './routes/user.routes.js';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
+// Lista de orígenes explícitos y regex para el dominio iglesiacci.com
 const allowedOrigins = [
     "https://asistencia-cci-front-dc13af93f3e4.herokuapp.com",
     "http://localhost:5173",
-    "http://localhost:3001",
-    /^(https?:\/\/)?(www\.)?iglesiacci\.com$/i
+    "http://localhost:3001"
 ];
+const allowedOriginRegex = /^(https?:\/\/)?(www\.)?iglesiacci\.com$/i;
 
 app.use(cors({
     origin: function (origin, callback) {
         // Permitir solicitudes sin 'origin' (como mobile apps o curl)
         if (!origin) return callback(null, true);
-        
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
+
+        // Validar origen en la lista o mediante regex
+        if (allowedOrigins.includes(origin) || allowedOriginRegex.test(origin)) {
+            return callback(null, true);
         }
-        return callback(null, true);
+
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
     },
     credentials: true
 }));
-
 
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -37,4 +39,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
 });
-
