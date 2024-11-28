19cbd5c6b46b6b2b9b89727de579d324ec4878f2
sebasmayor06 <sebda06@gmail.com>
2024-11-28 16:24:20 -0500

    agrego error si duplican el dni en asistencia

--- src/controllers/RegistroAsistencia.controllers.js
+++ src/controllers/RegistroAsistencia.controllers.js
@@ -4,13 +4,46 @@ import  pool  from "../db.js";
 
 export const registerAsistencia = async (req,res) =>{
 
-    console.log(req.body.nuevo);
-    
 
     const {dni, event_id, registration_time, update_asit, attended, aporte, peticion, nuevo, nombreinv} = req.body;
-    const sql = `INSERT INTO asistencia (dni, event_id, registration_time, update_asit, attended, aporte, peticion, nuevo, nombreinv) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`
-    
-    const {rows} = await pool.query(sql, [dni, event_id, registration_time, update_asit, attended, aporte, peticion, nuevo, nombreinv])
-    res.status(200).json(rows[0])
+    try {
+        const sql = `
+          INSERT INTO asistencia 
+            (dni, event_id, registration_time, update_asit, attended, aporte, peticion, nuevo, nombreinv) 
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
+          RETURNING *;
+        `;
+      
+        const { rows } = await pool.query(sql, [
+          dni,
+          event_id,
+          registration_time,
+          update_asit,
+          attended,
+          aporte,
+          peticion,
+          nuevo,
+          nombreinv,
+        ]);
+      
+        res.status(201).json({
+          message: "Registro creado exitosamente",
+          data: rows[0],
+        });
+      } catch (error) {
+        if (error.code === "23505") {
+          // Código de error para violación de restricción única
+          res.status(409).json({
+            message: "El registro ya existe",
+          });
+        } else {
+          console.error("Error al registrar asistencia:", error);
+          res.status(500).json({
+            message: "Error interno del servidor",
+            error: error.message,
+          });
+        }
+      }
+      
 
 }
